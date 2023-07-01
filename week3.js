97ee3dcaec59e82bd2e849bededc1eaf8c57010b
Aishat452 <aishadeperfectly@gmail.com>
2023-07-01 21:23:30 +0100

    done

---
+++ week3.js
@@ -0,0 +1,37 @@
+let bucketOne = 'water';
+let bucketTwo = 'sand';
+let bucketThree = 'milk';
+
+bucketOne = 'fire';
+
+console.log(bucketOne);
+// var
+const names = 'bisi';
+
+
+console.log(names);
+
+let greeting = "good morning";
+
+console.log(greeting);
+
+const a = "5";
+const b = 7;
+
+const addition = a + " " + b;
+console.log(addition);
+
+let x = 6.7; //float
+let y = 5; //int
+
+let result = x - y;
+
+console.log(result);
+
+let numbers = 21;
+let removal = 5;
+
+let result = numbers % removal;
+
+console.log(result);
+
